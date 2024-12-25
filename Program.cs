218694ed826e78410e46ab03cb2acbb8a0bd453a
Wiktor Radzikowski <wiktor.radzik102@gmail.com>
2024-12-25 18:02:06 +0100

    url as workshopID handling

--- Program.cs
+++ Program.cs
@@ -8,30 +8,52 @@ namespace SteamWorkshopBulkDownloader
 {
     class Data
     {
-        private string pathToFile = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".SteamWorksopBulkDownloader", "data.json");
-        public string steamCmdDirectory = String.Empty;
-
+        private readonly string appDirectory = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".SteamWorksopBulkDownloader");
+        private readonly string pathToFile = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".SteamWorksopBulkDownloader", "data.json");
+        public string? steamCmdDirectory { get; set; } = string.Empty;
 
         public void Save()
         {
+            ensureFileExists();
             string jsonString = JsonSerializer.Serialize(this);
+            Console.WriteLine(jsonString);
             try
             {
                 File.WriteAllText(pathToFile, jsonString);
             }
             catch (Exception e)
             {
-                Console.WriteLine("There was an error saving cache data, don't worry it doesn't affect downloads:\n" + e.Message + "\n");
+                Console.WriteLine($"There was an error saving cache data, don't worry it doesn't affect downloads:\n{e.Message}\n");
             }
         }
         public void Load()
         {
-            string jsonString = File.ReadAllText(pathToFile);
-            var deserialized = JsonSerializer.Deserialize<Data>(jsonString);
-            if (deserialized != null && !string.IsNullOrEmpty(deserialized.steamCmdDirectory))
+            ensureFileExists();
+            try
             {
-                if (!string.IsNullOrEmpty(deserialized.steamCmdDirectory))
-                    steamCmdDirectory = deserialized.steamCmdDirectory;
+                string jsonString = File.ReadAllText(pathToFile);
+                var deserialized = JsonSerializer.Deserialize<Data>(jsonString);
+                if (deserialized != null && !string.IsNullOrEmpty(deserialized.steamCmdDirectory))
+                {
+                    if (!string.IsNullOrEmpty(deserialized.steamCmdDirectory))
+                        steamCmdDirectory = deserialized.steamCmdDirectory;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"There was an error loading cache data, don't worry it doesn't affect downloads:\n{e.Message}\n");
+            }
+        }
+
+        private void ensureFileExists()
+        {
+            if (!Directory.Exists(appDirectory))
+            {
+                Directory.CreateDirectory(appDirectory);
+                if (!File.Exists(pathToFile))
+                {
+                    File.Create(pathToFile).Close();
+                }
             }
         }
     }
@@ -40,20 +62,54 @@ namespace SteamWorkshopBulkDownloader
     {
         static void Main()
         {
-            string cmdDir = String.Empty;
-            Console.WriteLine("SteamCMD directory: ");
-            cmdDir = Console.ReadLine();
-            string gameID = String.Empty;
+            var data = new Data();
+            data.Load();
+            string? gameID = string.Empty;
+            string? workshopID = string.Empty;
+
+            Console.WriteLine(data.steamCmdDirectory);
+
+            if (data.steamCmdDirectory == null || data.steamCmdDirectory == string.Empty)
+            {
+                Console.WriteLine("(folder path) SteamCMD directory: ");
+            }
+            else
+            {
+                Console.WriteLine($"Do you want to use recent steamCMD directory? ({data.steamCmdDirectory}) y/n (default: y)");
+                var ans = Console.ReadLine();
+                if (ans == "n")
+                {
+                    data.steamCmdDirectory = string.Empty;
+                    Console.WriteLine("(folder path) Input new SteamCMD directory: ");
+                }
+            }
+            while (data.steamCmdDirectory == null || data.steamCmdDirectory == string.Empty)
+            {
+                data.steamCmdDirectory = Console.ReadLine();
+            }
+
             Console.WriteLine("gameID: ");
-            gameID = Console.ReadLine();
+            while (gameID == null || gameID == string.Empty)
+            {
+                gameID = Console.ReadLine();
+            }
 
             while (true)
             {
-                string workshopID = String.Empty;
+                workshopID = string.Empty;
                 Console.WriteLine("workshopID: ");
-                workshopID = Console.ReadLine();
+                while (workshopID == null || workshopID == string.Empty)
+                {
+                    workshopID = Console.ReadLine();
+                    workshopID = unfoldURL(workshopID);
+                    if (workshopID?.ToLower() == "exit")
+                    {
+                        data.Save();
+                        return;
+                    }
+                }
 
-                StartBackgroundDownloadAsync(cmdDir, gameID, workshopID);
+                _ = StartBackgroundDownloadAsync(data.steamCmdDirectory, gameID, workshopID);
             }
         }
         static async Task StartBackgroundDownloadAsync(string directory, string gameID, string workshopID)
@@ -73,6 +129,27 @@ namespace SteamWorkshopBulkDownloader
 
                 await process.WaitForExitAsync();
             }
+        }
+
+        static string unfoldURL(string? url)
+        {
+            if (url == null) { return "0"; } // null check
+
+            int idStart = url.LastIndexOf('/') + 5;
+            int idEnd = idStart + 1;
+            for (int i = idEnd; i < url.Length; i++)
+            {
+                if (int.TryParse(url[i].ToString(), out _))
+                {
+                    idEnd = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return url.Substring(idStart, idEnd - idStart + 1);
         }
     }
 }
